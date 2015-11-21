7d586dd0bd5ea95362c55b004db209eff47b0358
190n <benjamin.j.grant@gmail.com>
2015-11-21 08:17:41 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream');
+
+gulp.task('default', function() {
+    gulp.src('lib/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('build'));
+    return browserify('build/main.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('build/'));
+});
